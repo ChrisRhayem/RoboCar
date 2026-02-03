776d995ab165dfee62dc254c4d15fbf3e5fbd3ee
Chris Rhayem <rhayemm.chris@gmail.com>
2026-02-03 13:17:05 +0100

    modification robocar

--- robocar.py
+++ robocar.py
@@ -5,23 +5,4 @@ class RoboCar(object):
         self.v = vitesse    # vitesse:int
         self.a = angle  # angle:int [0;360]
     
-    def orientation(self)->str:
-        """
-        Renvoie une chaîne de caractère donnant la direction de la voiture
-        """
-        if self.s%8 == 0:
-            return "↑"
-        elif self.s%8 == 1:
-            return "↗"
-        elif self.s%8 == 2:
-            return "→"
-        elif self.s%8 == 3:
-            return "↘"
-        elif self.s%8 == 4:
-            return "↓"
-        elif self.s%8 == 5:
-            return "↙"
-        elif self.s%8 == 6:
-            return "←"
-        elif self.s%8 == 7:
-            return "↖"
+    
