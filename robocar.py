3085b102596f4a425caacfd0f9242979ddd33b3f
Chris Rhayem <rhayemm.chris@gmail.com>
2026-02-07 19:56:35 +0100

    Creation de la fonction reculer

--- robocar.py
+++ robocar.py
@@ -1,9 +1,19 @@
+import math
 class RoboCar(object):
     def __init__(self, nom:str, coordonnees:tuple, vitesse:int, angle:int):
         self.n = nom    # nom:string
         self.coo = coordonnees  # coordonnees:tuple(int, int)
         self.v = vitesse    # vitesse:int
         self.a = angle  # angle:int [0;360]
-
-
-    
+    def avancer(self, vitesse):
+        """Avance la voiture selon son angle"""
+        angle_rad = math.radians(self.a)
+        x = self.coo[0] + math.cos(angle_rad) * vitesse
+        y = self.coo[1] + math.sin(angle_rad) * vitesse
+        self.coo = (x, y)
+    def reculer(self, vitesse):
+        """Recule la voiture selon son angle"""
+        angle_rad = math.radians(self.a)
+        x = self.coo[0] - math.cos(angle_rad) * vitesse
+        y = self.coo[1] - math.sin(angle_rad) * vitesse
+        self.coo = (x, y)
