c722f5a04422857f7305612b30770d37d87de73f
Chris Rhayem <rhayemm.chris@gmail.com>
2026-02-07 20:04:03 +0100

    Creation de la fonction Rotation

--- robocar.py
+++ robocar.py
@@ -13,7 +13,12 @@ class RoboCar(object):
         self.coo = (x, y)
     def reculer(self, vitesse):
         """Recule la voiture selon son angle"""
-        angle_rad = math.radians(self.a)
+        angle_rad = math.radians(self.a) #on met en radians pour que cos et sin puissent marcher 
         x = self.coo[0] - math.cos(angle_rad) * vitesse
         y = self.coo[1] - math.sin(angle_rad) * vitesse
         self.coo = (x, y)
+    def Rotation(self):
+        """Tourne la voiture sur elle-même"""
+        #si self.v est negatif alors flash tourne a gauche 
+        #si self.v est positif alors flash tourne a droite
+        self.a = (self.a + self.v) % 360 #on veut que l'angle reste entre 0 et 360 donc on fait un modulo 360
