0eaa024eba1a8a67188561d5119d28f6cfdcc358
Rim <rimbroush@gmail.com>
2026-02-09 17:14:17 +0100

    test carré deleted

--- affichage_pygame.py
+++ affichage_pygame.py
@@ -52,19 +52,6 @@ def main():
         if (flash.coo[0] < 0 or flash.coo[1] < 0 or
             flash.coo[0] + 50 > 500 or flash.coo[1] + 50 > 500):
             flash.coo = (old_x, old_y)
-        
-        trajet_carre = []
-        if keys[K_SPACE]:
-            trajet_carre = [("avancer", 100),
-                        ("tourner_droite", 90),
-                        ("avancer", 100),
-                        ("tourner_droite", 90),
-                        ("avancer", 100),
-                        ("tourner_droite", 90),
-                        ("avancer", 100)]
-        if trajet_carre:
-            flash.suivre_trajectoire(trajet_carre)
-        
 
         screen.fill((0, 0, 0))
         draw_flash(flash)
