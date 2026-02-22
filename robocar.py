25e2ddb058b0f80196fada21455089d6af271354
Chris Rhayem <rhayemm.chris@gmail.com>
2026-02-22 16:08:24 +0100

    update

--- robocar.py
+++ robocar.py
@@ -8,8 +8,8 @@ class RoboCar:
         self.angle = math.radians(angle) #orientation
 
         # vitesses roues
-        self.v_l = 0 #vitesse roue gauche
-        self.v_r = 0 #vitesse roue droite
+        self.vG = 0 #vitesse roue gauche
+        self.vR = 0 #vitesse roue droite
         self.largeur = 40   # largeur (cote roues)
         self.longueur = 60  # longueur (avant/arriere)
     def get_state(self):
@@ -18,18 +18,18 @@ class RoboCar:
 
     def get_wheel_speeds(self):
         """Recuperer la vitesse des roues"""
-        return self.v_l, self.v_r
+        return self.vG, self.vR
     def set_vitesse_gauche(self, v):
         """Modifier la vitesse du roue gauche"""
-        self.v_l = v
+        self.vG = v
 
     def set_vitesse_droite(self, v):
         """Modifier la vitesse du roue droite"""
-        self.v_r = v
+        self.vR = v
     def calculer_vitesse(self):
         """Cette fonction calcule la vitesse lineaire et angulaire"""
-        v = (self.v_r + self.v_l) / 2
-        w = (self.v_r - self.v_l) / self.WHEEL_BASE #c'est le theoreme de Thales applique au cercle de rotation
+        v = (self.vR + self.vG) / 2
+        w = (self.vR - self.vG) / self.WHEEL_BASE #c'est le theoreme de Thales applique au cercle de rotation
         return v, w
     def update(self, dt):
         """Mise a jour du robot"""
