6aec6aa786bf13a0ad116719a60af73736ac1084
IsabelaCaldeira <isabela.ribeiro@outlook.com>
2026-02-24 18:06:18 +0100

    updating documentation et optisation davancer_x-metres

--- simulation.py
+++ simulation.py
@@ -11,77 +11,80 @@ class Simulation():
     def update(voiture):
         return
 
-
-def get_wheel_speeds(voiture):
-    """Recuperer la vitesse des roues"""
-    return voiture.vG, voiture.vR
-def set_vitesse_gauche(voiture, v):
-    """Modifier la vitesse du roue gauche"""
-    voiture.vG = v
-def set_vitesse_droite(voiture, v):
-    """Modifier la vitesse du roue droite"""
-    voiture.vR = v
-def calculer_vitesse(voiture):
-    """Cette fonction calcule la vitesse lineaire et angulaire"""
-    v = (voiture.vR + voiture.vG) / 2
-    w = (voiture.vR - voiture.vG) / voiture.WHEEL_BASE #c'est le theoreme de Thales applique au cercle de rotation
-    return v, w
-
-def distance_obstacle(voiture, obstacles, max_range=120):
-    """Cette fonction regarde l'obstacle le plus proche"""
-    min_dist = max_range #distance minimale
-
-    # vecteur direction du voiture
-    dir_x = math.cos(voiture.angle)
-    dir_y = math.sin(voiture.angle)
+    def get_wheel_speeds(voiture):
+        """Recuperer la vitesse des roues"""
+        return voiture.vG, voiture.vR
     
-    for obs in obstacles:
-        ox, oy = obs.pos
-        dx = ox - voiture.x #on cree un vecteur du voiture vers l’obstacle
-        dy = oy - voiture.y
+    def set_vitesse_gauche(voiture, v):
+        """Modifier la vitesse du roue gauche"""
+        voiture.vG = v
+        
+    def set_vitesse_droite(voiture, v):
+        """Modifier la vitesse du roue droite"""
+        voiture.vR = v
+        
+    def calculer_vitesse(voiture):
+        """Cette fonction calcule la vitesse lineaire et angulaire"""
+        v = (voiture.vR + voiture.vG) / 2
+        w = (voiture.vR - voiture.vG) / voiture.WHEEL_BASE #c'est le theoreme de Thales applique au cercle de rotation
+        return v, w
 
-        # projection dans la direction du voiture
-        projection = dx * dir_x + dy * dir_y #produit scalaire
-        if 0 < projection < max_range: #on regard si l'obstacle est proche
-            dist = math.sqrt(dx**2 + dy**2)
-            if dist < min_dist:
-                min_dist = dist #on garde l'obstacle le plus proche devant
+    def distance_obstacle(voiture, obstacles, max_range=120):
+        """Cette fonction regarde l'obstacle le plus proche"""
+        min_dist = max_range #distance minimale
 
-    return min_dist
+        # vecteur direction du voiture
+        dir_x = math.cos(voiture.angle)
+        dir_y = math.sin(voiture.angle)
+        
+        for obs in obstacles:
+            ox, oy = obs.pos
+            dx = ox - voiture.x #on cree un vecteur du voiture vers l’obstacle
+            dy = oy - voiture.y
 
-def distance_mur(voiture, largeur, hauteur, max_range=120):
-    """Cette fonction renvoie la distance au mur le plus proche dans la direction du voiture"""
-     # point devant le voiture
-    front_x = voiture.x + math.cos(voiture.angle) * max_range #on avance de 120 pixels dans la direction du voiture
-    front_y = voiture.y + math.sin(voiture.angle) * max_range
+            # projection dans la direction du voiture
+            projection = dx * dir_x + dy * dir_y #produit scalaire
+            if 0 < projection < max_range: #on regard si l'obstacle est proche
+                dist = math.sqrt(dx**2 + dy**2)
+                if dist < min_dist:
+                    min_dist = dist #on garde l'obstacle le plus proche devant
 
-    # distance au mur le plus proche
-    dist_x = min(front_x, largeur - front_x)
-    dist_y = min(front_y, hauteur - front_y)
+        return min_dist
 
-    return min(dist_x, dist_y)
+    def distance_mur(voiture, largeur, hauteur, max_range=120):
+        """Cette fonction renvoie la distance au mur le plus proche dans la direction du voiture"""
+        # point devant le voiture
+        front_x = voiture.x + math.cos(voiture.angle) * max_range #on avance de 120 pixels dans la direction du voiture
+        front_y = voiture.y + math.sin(voiture.angle) * max_range
 
-def obtenir_rectangle(voiture):
-    """cette fonction cree un rectangle simplifie autour du voiture pour faire les collisions"""
-    half_L = voiture.longueur / 2 #le voiture est centre donc on calcule le centre pour le retrancher apres a x et y
-    half_W = voiture.largeur / 2
+        # distance au mur le plus proche
+        dist_x = min(front_x, largeur - front_x)
+        dist_y = min(front_y, hauteur - front_y)
 
-    return (
-        voiture.x - half_L, #on va du centre vers la gauche
-        voiture.y - half_W, #on va du centre vers le haut
-        voiture.longueur,
-        voiture.largeur
-    )
+        return min(dist_x, dist_y)
 
-def collision(voiture, obstacle):
-    """Cette fonction detetcte la collision"""
-    x1, y1, w1, h1 = voiture.obtenir_rectangle()
-    x2, y2 = obstacle.pos
-    w2, h2 = obstacle.dim
+    def obtenir_rectangle(voiture):
+        """cette fonction cree un rectangle simplifie autour du voiture pour faire les collisions"""
+        half_L = voiture.longueur / 2 #le voiture est centre donc on calcule le centre pour le retrancher apres a x et y
+        half_W = voiture.largeur / 2
 
-    return (
-        x1 < x2 + w2 and
-        x1 + w1 > x2 and
-        y1 < y2 + h2 and
-        y1 + h1 > y2
-    )
+        return (
+            voiture.x - half_L, #on va du centre vers la gauche
+            voiture.y - half_W, #on va du centre vers le haut
+            voiture.longueur,
+            voiture.largeur
+        )
+
+    def collision(voiture, obstacle):
+        """Cette fonction detetcte la collision avec les 
+        dimensitions complete (pas seulmenet son centre) du robot avec l'aide de obternir_retangle"""
+        x1, y1, w1, h1 = voiture.obtenir_rectangle()
+        x2, y2 = obstacle.pos
+        w2, h2 = obstacle.dim
+
+        return (
+            x1 < x2 + w2 and
+            x1 + w1 > x2 and
+            y1 < y2 + h2 and
+            y1 + h1 > y2
+        )
--- strategies.py
+++ strategies.py
@@ -22,10 +22,10 @@ class Deplacement:
             sim.set_vitesse_gauche(self.robot, 30)
             sim.set_vitesse_droite(self.robot, 30)
             
-    def avance(self):
+    def avancer(self, vitesse):
         """Permet Flash d'avancer"""
-        sim.set_vitesse_gauche(self.robot, 80)
-        sim.set_vitesse_droite(self.robot, 80)
+        sim.set_vitesse_gauche(self.robot, vitesse)
+        sim.set_vitesse_droite(self.robot, vitesse)
         
     def arreter(self):
         """met la vitesse des roues à 0 pour arrêter le robot"""
@@ -37,18 +37,19 @@ class Deplacement:
         distance_pixels = distance * 100 # Convertir la distance en pixels
         if self.depart is None: #Si c'est le premier appel, on memorise le point de depart
             self.depart = self.robot.get_state()
+        
         #on avance tout droit
-        sim.set_vitesse_gauche(self.robot, vitesse)
-        sim.set_vitesse_droite(self.robot, vitesse)
+        self.avancer(self.robot)
         self.robot.update(dt) #on met à jour la position du robot
+        
         #distance déjà parcourue depuis le départ
         dx = - self.robot.x + self.depart[0]
         dy = self.robot.y - self.depart[1]
-        distance_parcourue = math.sqrt(dx**2 + dy**2)
+        distance_parcourue = math.sqrt(dx**2 + dy**2
+                                       )
         #si on a atteint la distance cible, on arrete le robot
         if distance_parcourue >= distance_pixels:
-            sim.set_vitesse_gauche(self.robot, 0)
-            sim.set_vitesse_droite(self.robot, 0)
+            self.arreter(self.robot, vitesse)
             self.depart = None #on reset le point de départ
             return True #la distance a été atteinte
         return False #le robot continue d'avancer
