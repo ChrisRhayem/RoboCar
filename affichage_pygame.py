dc45367599ad93e08f90c5373e4d1acb252839d7
Chris Rhayem <rhayemm.chris@gmail.com>
2026-02-24 20:07:54 +0100

    test de avancer_x_metre

--- affichage_pygame.py
+++ affichage_pygame.py
@@ -60,13 +60,16 @@ def main():
     sim = Simulation(robot, obstacles, LARGEUR, HAUTEUR) #on cree la simulation qui contient le robot,les obstacles
     strat = Deplacement(sim) #on cree la stratégie qui reçoit la simulation
     running = True
+    mouvement_lineaire= False
     while running:
         dt = clock.tick(FPS) / 1000.0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        strat.eviter_obstacles(vitesse_avance=80, vitesse_tourne=60, seuil=70) #on decide quoi faire (avancer,tourner) le robot ne bouge pas la mais on regle sa vitesse seulement
-
+        if not mouvement_lineaire:
+            mouvement_lineaire=strat.avancer_x_metres(2, 80)
+        else:
+            strat.eviter_obstacles(80, 60, 70) #on decide quoi faire (avancer,tourner) le robot ne bouge pas la mais on regle sa vitesse seulement
         a_collision = sim.update(dt) #c'est a que le robot bouge réellement
         if a_collision:
             strat.tourner_sur_place(60)
