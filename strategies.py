6d8da1424b9d4632d4f322773fc9e5ffd048b134
Rim <rimbroush@gmail.com>
2026-02-24 13:29:22 +0100

    utiliser les fonctions de simulation pour l'evitement et le controle des vitesses

--- strategies.py
+++ strategies.py
@@ -1,3 +1,4 @@
+import simulation as sim
 class Deplacement:
     def __init__(self, robot, obstacles):
         self.robot = robot
@@ -6,28 +7,28 @@ class Deplacement:
     #Eviter les obstacles il faut reviser
     def eviter_obstacles(self, dt):
         """Met à jour les vitesses des roues pour avancer et éviter les obstacles."""
-        dist_obs = self.robot.distance_obstacle(self.obstacles) #la distance au plus proche obstacle devant le robot
-        dist_mur = self.robot.distance_mur(800, 600) #la distance au mur le plus proche
-
+        dist_obs = sim.distance_obstacle(self.robot, self.obstacles) #la distance au plus proche obstacle devant le robot
+        dist_mur = sim.distance_mur(self.robot, 800, 600) #la distance au mur le plus proche
         distance = min(dist_obs, dist_mur)
 
         if distance < 40:
             # tourne
-            self.robot.set_vitesse_gauche(-60)
-            self.robot.set_vitesse_droite(60)
+            sim.set_vitesse_gauche(self.robot, -60)
+            sim.set_vitesse_droite(self.robot, 60)
         else:
             # avance
-            self.robot.set_vitesse_gauche(30)
-            self.robot.set_vitesse_droite(30)
+            sim.set_vitesse_gauche(self.robot, 30)
+            sim.set_vitesse_droite(self.robot, 30)
             
     def avance(self):
         """Permet Flash d'avancer"""
-        self.robot.set_vitesse_gauche(80)
-        self.robot.set_vitesse_droite(80)
+        sim.set_vitesse_gauche(self.robot, 80)
+        sim.set_vitesse_droite(self.robot, 80)
         
     def arreter(self):
-        self.robot.set_vitesse_gauche(80)
-        self.robot.set_vitesse_droite(80)
+        """met la vitesse des roues à 0 pour arrêter le robot"""
+        sim.set_vitesse_gauche(self.robot, 0)
+        sim.set_vitesse_droite(self.robot, 0)
     
         
             
