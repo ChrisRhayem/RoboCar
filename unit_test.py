65287ce7675fbd76be488320f4c2cbe816d26ed3
Chris Rhayem <rhayemm.chris@gmail.com>
2026-03-14 18:55:48 +0100

    test de simulation.py

--- unit_test.py
+++ unit_test.py
@@ -40,3 +40,49 @@ class TestRoboCar(unittest.TestCase):
 
         self.assertAlmostEqual(self.robot.x, 110)
         self.assertAlmostEqual(self.robot.y, 200)
+
+
+class TestSimulation(unittest.TestCase):
+
+    def setUp(self):
+        self.sim = Simulation(800, 600)
+
+    def test_avancer(self):
+        self.sim.avancer(80)
+
+        self.assertEqual(self.sim.robot.vG, 80)
+        self.assertEqual(self.sim.robot.vR, 80)
+
+    def test_reculer(self):
+        self.sim.reculer(40)
+
+        self.assertEqual(self.sim.robot.vG, -40)
+        self.assertEqual(self.sim.robot.vR, -40)
+
+    def test_tourner_sur_place(self):
+        self.sim.tourner_sur_place(50)
+
+        self.assertEqual(self.sim.robot.vG, 50)
+        self.assertEqual(self.sim.robot.vR, -50)
+
+    def test_freiner(self):
+        self.sim.robot.vG = 100
+        self.sim.robot.vR = 100
+
+        self.sim.freiner(1)
+
+        self.assertEqual(self.sim.robot.vG, 0)
+        self.assertEqual(self.sim.robot.vR, 0)
+
+    def test_distance_obstacle_positive(self):
+        dist = self.sim.distance_obstacle()
+        self.assertGreaterEqual(dist, 0)
+
+    def test_obtenir_rectangle(self):
+        rect = self.sim.obtenir_rectangle()
+        self.assertEqual(len(rect), 4)
+
+    def test_collision_type(self):
+        obs = self.sim.obstacles[0]
+        resultat = self.sim.collision(obs)
+        self.assertIsInstance(resultat, bool)
